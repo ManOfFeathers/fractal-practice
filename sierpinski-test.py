f5290adf9e2b5ba5d3ca85b7aaf3750d3c161fb0
ManOfFeathers <griffintmyers@gmail.com>
2017-06-24 17:21:23 -0600

    Created trema

--- sierpinski-test.py
+++ sierpinski-test.py
@@ -1,14 +1,26 @@
+#                 Sierpinski Triangle
+#
+#  Programmed by Jean Flaherty and Griffin Myers (06-24-2017)
+#
+#  The means of implementing the functions from sierpinski.py in a
+#  TkInter canvas
+#
+
 from sierpinski import Triangle
 from tkinter import *
 import math
 
+width = 729
+height = 729
+
 root = Tk()
 
-c = Canvas(root, width = 500, height = 500, bg = "white")
-c.pack(expand= YES, fill = BOTH)
+c = Canvas(root, width = 729, height = 729, bg = "white")
+c.pack(expand = YES, fill = BOTH)
 
-triangle = Triangle(root, c, left = 0, top = 0, width = 500, height = 500,
+triangle = Triangle(root, c, left = 0, top = 0, width = width, height = height,
                     obj_color = "black", trema_color = "white")
 
-triangle.draw()
+triangle.draw(0,0,width)
+
 c.update()
--- sierpinski.py
+++ sierpinski.py
@@ -1,3 +1,17 @@
+#                 Sierpinski Triangle
+#
+#  Programmed by Jean Flaherty and Griffin Myers (06-24-2017)
+#
+#  A couple of ways to draw the Sierpinski triangle, another image with
+#  an infinitely recursive pattern
+#
+#  Functions:
+#       __init__  -- introduce attributes
+#       draw      -- recursive fuction that draws the original triangle,
+#                    as well as each trema, layer by layer
+#
+
+
 from tkinter import *
 from PIL import ImageGrab
 import math
@@ -27,58 +41,95 @@ class Triangle:
         self.center = (self.left + self.right) / 2.0
         self.middle = (self.top + self.bottom) / 2.0
 
-        self.depth = 3
+        #self.depth = 5
+        
 
-        #self.draw()
+##    def draw(self):
+##
+##        queue = [((self.left,self.bottom), (self.right,self.bottom), (self.center,self.top))]
+##
+##        depth = 0
+##
+##        while queue:
+##            for i in range(3 ** depth):
+##
+##                (left_tup, right_tup, top_tup) = queue.pop(0)
+##
+##                (left_x, left_y) = left_tup
+##                (right_x, right_y) = right_tup
+##                (top_x, top_y) = top_tup
+##
+##                width = right_x - left_x
+##                height = (width * math.sqrt(3)) / 2.0 
+##
+##                center = (left_x + right_x) / 2.0
+##
+##                bottom = height
+##
+##                triangle = (    (center,top_y), (right_x,bottom), (left_x,bottom)  )
+##
+##                trema = (   (left_x+(width/2),bottom-(height/2)),
+##                            (right_x-(width/2),bottom-(height/2)),
+##                            (width/2, bottom)    )
+##
+##                self.triangle_id = self.c.create_polygon(triangle, fill = self.obj_color, tag=self.tag)
+##
+##                self.trema_id = self.c.create_polygon(trema, fill = self.trema_color, tag=self.tag)
+##
+##                queue.append((
+##                            (left_x+(width/8),bottom-(height/4)),
+##                            (center-(width/8),bottom-(height/4)),
+##                            (width/4, bottom)
+##                            ))
+##
+####                queue.append((top+(height/3), bottom-(height/3), right-(width/3), right))
+####                queue.append((bottom-(height/3), bottom, right-(width/3), right))
+####                queue.append((bottom-(height/3), bottom, left+(width/3), right-(width/3)))
+####                queue.append((bottom-(height/3), bottom, left, left+(width/3)))
+####                queue.append((top+(height/3), bottom-(height/3), left, left+(width/3)))
+##
+##            self.c.update()
+##            self.c.after(1000)
+##            self.screenshot(self.c, "tri_{}.gif".format(depth))
+##            depth += 1
 
-    def draw(self):
+    def draw(self, top, left, right):
 
-        queue = [((self.left,self.bottom), (self.right,self.bottom), (self.center,self.top))]
-
+        queue = [(top, left, right)]
         depth = 0
-
         while queue:
             for i in range(3 ** depth):
+                (top, left, right) = queue.pop(0)
 
-                (left_tup, right_tup, top_tup) = queue.pop(0)
+                width = right - left
 
-                (left_x, left_y) = left_tup
-                (right_x, right_y) = right_tup
-                (top_x, top_y) = top_tup
+                height = (width * math.sqrt(3)) / 2.0
 
-                width = right_x - left_x
-                height = (width * math.sqrt(3)) / 2.0 
-
-                center = (left_x + right_x) / 2.0
+                center = (left + right) / 2.0
 
                 bottom = height
 
-                triangle = (    (center,top_y), (right_x,bottom), (left_x,bottom)  )
+                triangle = (    (center,top), (right,bottom), (left,bottom)   )
 
-                trema = (   (left_x+(width/2),bottom-(height/2)),
-                            (right_x-(width/2),bottom-(height/2)),
-                            (width/2, bottom)    )
+                trema = (   (left+(width/4),height/2),
+                            (right-(width/4),height/2),
+                            (center, bottom)   )
 
                 self.triangle_id = self.c.create_polygon(triangle, fill = self.obj_color, tag=self.tag)
 
                 self.trema_id = self.c.create_polygon(trema, fill = self.trema_color, tag=self.tag)
 
                 queue.append((
-                            (left_x+(width/8),bottom-(height/4)),
-                            (center-(width/8),bottom-(height/4)),
-                            (width/4, bottom)
+                            (left+(width/2),bottom-(height/2)),
+                            (center-(width/2),bottom-(height/2)),
+                            (width/2, bottom)
                             ))
-
-##                queue.append((top+(height/3), bottom-(height/3), right-(width/3), right))
-##                queue.append((bottom-(height/3), bottom, right-(width/3), right))
-##                queue.append((bottom-(height/3), bottom, left+(width/3), right-(width/3)))
-##                queue.append((bottom-(height/3), bottom, left, left+(width/3)))
-##                queue.append((top+(height/3), bottom-(height/3), left, left+(width/3)))
-
+                
             self.c.update()
             self.c.after(1000)
             self.screenshot(self.c, "tri_{}.gif".format(depth))
             depth += 1
+                
 
     # https://stackoverflow.com/questions/9886274/how-can-i-convert-canvas-content-to-an-image
     def screenshot(self, widget, filename):
