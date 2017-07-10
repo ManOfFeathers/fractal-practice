55fb6e54ddb6d6bdeb76be83331e9dfd57f8de76
ManOfFeathers <griffintmyers@gmail.com>
2017-07-10 16:00:38 -0600

    Cleaned up output

--- cantor.py
+++ cantor.py
@@ -9,6 +9,7 @@
 
 import numpy as np
 from tkinter import *
+from PIL import ImageGrab
 
 root = Tk()
 
@@ -25,6 +26,7 @@ def cantor(max_i, x_start, x_end, y):
             (x_end,y)]]
     mat_r = x_end - x_start 
 
+    depth = 0
     while queue:
         coord = queue.pop(0)
         c.create_line(coord[0][0],coord[0][1], coord[1][0],coord[1][1], width = 2, fill = "black")
@@ -35,7 +37,18 @@ def cantor(max_i, x_start, x_end, y):
         queue.append(coord1)
         queue.append(coord2)
         c.update()
-        c.after(1000)
+##        c.after(1000)
+        screenshot(c, "cantor_{}.gif".format(depth))
+        depth += 1
+
+# https://stackoverflow.com/questions/9886274/how-can-i-convert-canvas-content-to-an-image
+def screenshot(widget, filename):
+
+    x = root.winfo_rootx() + widget.winfo_x()
+    y = root.winfo_rooty() + widget.winfo_y()
+    x1 = x + widget.winfo_width()
+    y1 = y + widget.winfo_height()
+    ImageGrab.grab().crop((x,y,x1,y1)).save(filename)
 
 cantor(4, 0, width, 6)  # call the function without the old tester code
 c.update()
